a5f7e67f7636e91ebc346bfa614a8501e43638e8
Thomas Rolinger <tbrolin@bluethree.lps.umd.edu>
2018-04-27 15:33:31 +0000

    Added EMU hardware parse script

--- scripts/parse_EMU-HW_metrics.py
+++ scripts/parse_EMU-HW_metrics.py
@@ -5,11 +5,11 @@ import os
     Author: Thomas Rolinger (tbrolin@cs.umd.edu)
     Date: 4/27/2018
 
-    Parses the output from the CPU code. What we're gathering are the
-    total cycles and various cache load misses. This script will average
-    these values for each data set and produce an output CSV of the form:
+    Parses the output from the EMU (hardware) code. What we're gathering are the
+    total cycles. This script will average these values for each data set and produce 
+    an output CSV of the form:
 
-    NUM ELEMENTS, TOTAL CYCLES, L1 LDM, L2 LDM, L3 LDM
+    NUM ELEMENTS, TOTAL CYCLES
 
     The arguments to the script are the following:
 
@@ -18,7 +18,7 @@ import os
 
     2.) The name of the output CSV file
 
-    usage: python parse_CPU_metrics.py output_data/ myOutput.csv
+    usage: python parse_EMU-HW_metrics.py output_data/ myOutput.csv
 '''
 
 def getSubDirs(myDir):
@@ -40,7 +40,7 @@ if __name__ == "__main__":
     fOut = open(outFile, "w")
 
     # Write out header. 
-    fOut.write("NUM ELEMENTS,TOTAL CYCLES,L1 LDM,L2 LDM,L3 LDM\n")
+    fOut.write("NUM ELEMENTS,TOTAL CYCLES\n")
     # Get the subdirs in the top level dir. These should be the small, medium, large.
     # We want to order them as small, medium, large (happens to be reverse alphabetical)
     subDirs = getSubDirs(topDir)
@@ -64,9 +64,6 @@ if __name__ == "__main__":
             # If no files, skip
             if numFiles != 0:
                 TOT_CYC = 0.0
-                L1_LDM = 0.0
-                L2_LDM = 0.0
-                L3_LDM = 0.0
                 # Parse each file
                 for trialFile in trialFiles:
                     try:
@@ -75,16 +72,9 @@ if __name__ == "__main__":
                         fTrial.close()
                         # Get total cycles
                         TOT_CYC += [int(line.split("TOTAL CYCLES:")[1].strip()) for line in lines if "TOTAL CYCLES:" in line][0]
-                        # Get cache load misses
-                        L1_LDM += [int(line.split("L1 LOAD MISSES:")[1].strip()) for line in lines if "L1 LOAD MISSES:" in line][0]
-                        L2_LDM += [int(line.split("L2 LOAD MISSES:")[1].strip()) for line in lines if "L2 LOAD MISSES:" in line][0] 
-                        L3_LDM += [int(line.split("L3 LOAD MISSES:")[1].strip()) for line in lines if "L3 LOAD MISSES:" in line][0]
                     except Exception, e:
                         print("**** FAILED to process file %s" %trialFile)
                         print("\t%s" %str(e))
                 # Output data to CSV
-                fOut.write("%d,%d,%d,%d,%d\n" %(int(numElements), int(TOT_CYC/numFiles), int(L1_LDM/numFiles), int(L2_LDM/numFiles), int(L3_LDM/numFiles)))
-                
-
-
+                fOut.write("%d,%d\n" %(int(numElements), int(TOT_CYC/numFiles))))
     fOut.close()
